4e6dac1ba5f630ebf7804f535e71f7d222588576
Faisd405 <faisd405@gmail.com>
2022-05-16 16:10:14 +0700

    fix maps create

--- public/js/here.js
+++ public/js/here.js
@@ -1,36 +1,35 @@
-if (navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position => {
-        localCoord = position.coords;
-        objLocalCoord = {
-            lat: localStorage.latitude,
-            lng: localStorage.longitude
-        }
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(position => {
+    localCoord = position.coords;
+    objLocalCoord = {
+      lat: localCoord.latitude,
+      lng: localCoord.longitude
+    }
 
-        
-      // Initialize the platform object
-      var platform = new H.service.Platform({
-        'apikey': window.hereApiKey
+    // Initialize the platform object
+    var platform = new H.service.Platform({
+      'apikey': window.hereApiKey
+    });
+
+    // Obtain the default map types from the platform object
+    let defaultLayers = platform.createDefaultLayers();
+
+    // Instantiate (and display) the map
+    var map = new H.Map(
+      document.getElementById('mapContainer'),
+      defaultLayers.vector.normal.map,
+      {
+        zoom: 13,
+        center: objLocalCoord,
+        pixelRatio: window.devicePixelRatio || 1
       });
+    window.addEventListener('resize', () => map.getViewPort().resize());
 
-      // Obtain the default map types from the platform object
-      let defaultLayers = platform.createDefaultLayers();
+    let ui = H.ui.UI.createDefault(map, defaultLayers);
+    let mapEvents = new H.mapevents.MapEvents(map);
+    let behavior = new H.mapevents.Behavior(mapEvents);
 
-      // Instantiate (and display) the map
-      var map = new H.Map(
-        document.getElementById('mapContainer'),
-        defaultLayers.vector.normal.map,
-        {
-          zoom: 13,
-          center: objLocalCoord,
-          pixelRatio: window.devicePixelRatio || 1
-        });
-        window.addEventListener('resize', () => map.getViewPort().resize());
-
-        let ui = H.ui.UI.createDefaultLayers(map, defaultLayers);
-        let mapEvents = new H.mapevents.MapEvents(map);
-        let behavior = new H.mapevents.Behavior(mapEvents);
-
-    })
+  })
 } else {
-    console.log('Geolocation is not supported by this browser.');
+  console.log('Geolocation is not supported by this browser.');
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,9 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
     <script src="https://js.api.here.com/v3/3.1/mapsjs-core.js" type="text/javascript" charset="utf-8"></script>
     <script src="https://js.api.here.com/v3/3.1/mapsjs-service.js" type="text/javascript" charset="utf-8"></script>
     <script src="https://js.api.here.com/v3/3.1/mapsjs-ui.js" type="text/javascript" charset="utf-8"></script>
