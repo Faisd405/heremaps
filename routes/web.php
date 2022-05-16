207cdbc2478626e6e183873cb26ecda7d67e990a
Faisd405 <faisd405@gmail.com>
2022-05-16 19:15:38 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SpaceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('/space', App\Http\Controllers\SpaceController::class);
+Route::get('/space', [SpaceController::class, 'index'])->name('space.index');
+Route::get('/space/create', [SpaceController::class, 'create'])->name('space.create');
+Route::post('/space', [SpaceController::class, 'store'])->name('space.store');
+Route::get('/space/{space}', [SpaceController::class, 'show'])->name('space.show');
+Route::get('/space/{space}/edit', [SpaceController::class, 'edit'])->name('space.edit');
+Route::put('/space/{space}', [SpaceController::class, 'update'])->name('space.update');
+Route::delete('/space/{space}', [SpaceController::class, 'destroy'])->name('space.destroy');
